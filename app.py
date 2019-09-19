d83765720010cc8bdace8f789a0909a62b48f4e3
MiguiTE <miguite19@gmail.com>
2019-09-19 15:14:53 +0200

    Added functionality
    Added functionality to delete a repository and make it public

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import sys
 import os
 
@@ -6,12 +7,36 @@ from requests.auth import HTTPBasicAuth
 from credentials import user, password
 
 # usefull vars
-repo = sys.argv[1]
+command = sys.argv[1]
+repo = sys.argv[2]
 
+'''Function that creates a repository private by default'''
+def create(repo, user, password):
+    response = requests.post(
+        "https://api.github.com/user/repos",
+        auth=HTTPBasicAuth(user, password),
+        json={"name": repo, "private": True}
+    )
 
-# create repository
-response = requests.post(
-    "https://api.github.com/user/repos",
-    auth=HTTPBasicAuth(user, password),
-    json={"name": repo, "private": True}
-)
+'''Function that deletes a repository'''
+def delete(repo, user, password):
+    response = requests.delete(
+        "https://api.github.com/repos/{}/{}".format(user,repo),
+        auth=HTTPBasicAuth(user, password)
+    )
+
+'''Function that makes a repository public'''
+def makePublic(repo, user, password):
+    response = requests.patch(
+        "https://api.github.com/repos/{}/{}".format(user,repo),
+        auth=HTTPBasicAuth(user, password),
+        data=json.dumps({"private": False})
+    )
+
+# Manage user input
+if (command == "create"):
+    create(repo, user, password)
+elif (command == "delete"):
+    delete(repo, user, password)
+elif (command == "makePublic"):
+    makePublic(repo, user, password)
--- app.sh
+++ app.sh
@@ -1,17 +1,54 @@
 #!/bin/bash
-repo=${1}
+
+# Function that graps the creation of a flutter project and git repository
+# it starts visual studio code
+create () {
+    #flutter create ${dir}${repo} # create flutter project
+    mkdir ${dir}${repo}
+    python ${dir}"automaticGit/app.py" ${command} ${repo} # create git repository
+
+    # sincronize git repository
+    cd ${dir}${repo}
+    git init
+    echo "# "${repo} > README.md
+    git add *
+    git commit -m "first commit"
+    git remote add origin https://github.com/${user}/${repo}.git
+    git push -u origin master
+    code .
+}
+
+# Delete git repository and flutter project directory
+delete () {
+    python ${dir}"automaticGit/app.py" ${command} ${repo}
+    cd ${dir}
+    rm ${dir}${repo} -rf
+}
+
+# Make the repository public
+makePublic () {
+    python ${dir}"automaticGit/app.py" ${command} ${repo}
+}
+
+# Get command and repo name
+command=${1}
+repo=${2}
+
+# Set flutter projects directory
 dir="/home/"${USER}"/workspace/flutter/"
 
-source ${dir}"automaticGit/credentials.py"
+# Get credentials
+source ${dir}"automaticGit/credentials.py" 
 
-python ${dir}"automaticGit/app.py" ${repo}
+# Manage user input
+if [ $command == "create" ]; then
+    create
+fi
 
-mkdir ${dir}${repo}
-cd ${dir}${repo}
-git init
-echo "# "${repo} > README.md
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/${user}/${repo}.git
-git push -u origin master
-code .
+if [ $command == "delete" ]; then
+    delete
+fi
+
+if [ $command == "makePublic" ]; then
+    makePublic
+fi
